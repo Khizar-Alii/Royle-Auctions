433f7997a0f1848fbc8e3629f9aea825beeb8553
Khizar Ali <khizibutt123@gmail.com>
2024-05-13 09:59:13 +0500

    Updated text in homePage

--- src/HomeComponents/HLife/HLife.jsx
+++ src/HomeComponents/HLife/HLife.jsx
@@ -10,13 +10,34 @@ const HLife = () => {
           <img src="./HLife.jpg" alt="HLife" />
         </div>
         <div className={styles.HLifeContainerMid}>
-          <h3>TALK: PAOLO <br /> TORMENA</h3>
+          <h3>
+            We are proud antique collectors and have a huge selection of 19th
+            and 18th century <br /> European antique furniture
+          </h3>
           <p>
-          Milan is an international hub for many industries. In the design industry, Milan is the hub. S34 (showroom Henge in via Della Spiga 34 - editor's note) is our clients' home, a space where we don't sell anything, but where we welcome all our worldwide partners and their clients to explore our collection. Our objective was to create an original and unconventional interior design project by filling up volumes with the mood that characterizes us.
+            Royal Auction House specializes in unique vintage furniture and
+            accessories designed to imbue, luxury, sophistication and unique
+            character to your home. Living and breathing decorative, industrial,
+            mid-century, and European, Egyptian and Asian antiques, introduce
+            imaginative style to both period and contemporary decor settings. We
+            source a complete range of vintage furniture and decorative objects
+            in a wide range of ages and styles.
           </p>
         </div>
         <div className={styles.HLifeContainerRight}>
-            <p>The task hasn't been easy because the space has three levels with completely different architectural characteristics in each one. However, we decided to take this as plus rather than a challenge. The investment is significant, but we think that we can leverage our position and attractiveness to welcome our guests throughout the year and not just for the Design Week.</p>
+          <p>
+            Our key objective is that our
+            items work harmoniously within a contemporary design scheme, adding
+            soul and style to your home, shop, cafe, restaurant, chalet, or
+            boutique hotel. Established in 2004, the Royal House of Auctions
+            strives to find the perfect pieces to complete your most imaginative
+            interior designs.
+            <br />
+            If there's a special something you'd like, you can commission us to
+            find it for you ! Or if you have any questions, we are happy to
+            discuss all the details. Just email us at{" "}
+            <strong>Khaled@roualautionhouse.com </strong>
+          </p>
         </div>
       </div>
     </div>
