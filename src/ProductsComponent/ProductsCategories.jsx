105e5e38bbcf66773b21501fae709cffefc87f46
Khizar Ali <khizibutt123@gmail.com>
2024-05-16 16:11:52 +0500

    Added more images in ligtening category

--- src/ProductsComponent/ProductsCategories.jsx
+++ src/ProductsComponent/ProductsCategories.jsx
@@ -1,222 +1,346 @@
-export const  productCategories = [
-    {
-      path: "/center-piece",
-      name: "Center Piece",
-      pic: "./centerPiece.jpg",
-      id: 1,
-    },
-    {
-      path: "/clocks",
-      name: "Clocks",
-      pic: "./clocks.jpg",
-      id: 2,
-    },
-    {
-      path: "/lighting",
-      name: "Lighting",
-      pic: "./lighting.jpg",
-      id: 3,
-    },
-    {
-      path: "/mirrors",
-      name: "Mirrors",
-      pic: "./mirrors.jpg",
-      id: 4,
-    },
-    {
-      path: "/porcelain",
-      name: "Porcelain",
-      pic: "porcelain.jpg",
-      id: 5,
-    },
-    {
-      path: "/paintings",
-      name: "Paintings",
-      pic: "./painting.jpg",
-      id: 6,
-    },
-    {
-      path: "/seating",
-      name: "Seating",
-      pic: "./seating.jpg",
-      id: 7,
-    },
-    {
-      path: "/storage",
-      name: "Storage",
-      pic: "./storage.jpg",
-      id: 8,
-    },
-    {
-      path: "/sculpture",
-      name: "Sculpture",
-      pic: "./sculpture.jpg",
-      id: 9,
-    },
-    {
-      path: "/silverwave",
-      name: "Silverwave",
-      pic: "Silverware.jpg",
-      id: 10,
-    },
-    {
-      path: "/tables",
-      name: "Tables",
-      pic: "./tables.jpg",
-      id: 11,
-    },
-    {
-      path: "/dining-room",
-      name: "Dining Room",
-      pic: "./diningRoom.jpg",
-      id: 12,
-    },
-    {
-      path: "/living-room",
-      name: "Living Room",
-      pic: "./livingRoom.jpg",
-      id: 13,
-    },
-    {
-      path: "/marbles",
-      name: "Marbles",
-      pic: "./marbles.jpg",
-      id: 14,
-    },
-    {
-      path: "/bed-room",
-      name: "Bed Room",
-      pic: "./bedroom.jpg",
-      id: 15,
-    },
-    {
-      path: "/crystal",
-      name: "Crystal",
-      pic: "./crystal.jpg",
-      id: 16,
-    },
-    
+export const productCategories = [
+  {
+    path: "/center-piece",
+    name: "Center Piece",
+    pic: "./centerPiece.jpg",
+    id: 1,
+  },
+  {
+    path: "/clocks",
+    name: "Clocks",
+    pic: "./clocks.jpg",
+    id: 2,
+  },
+  {
+    path: "/lighting",
+    name: "Lighting",
+    pic: "./lighting.jpg",
+    id: 3,
+  },
+  {
+    path: "/mirrors",
+    name: "Mirrors",
+    pic: "./mirrors.jpg",
+    id: 4,
+  },
+  {
+    path: "/porcelain",
+    name: "Porcelain",
+    pic: "porcelain.jpg",
+    id: 5,
+  },
+  {
+    path: "/paintings",
+    name: "Paintings",
+    pic: "./painting.jpg",
+    id: 6,
+  },
+  {
+    path: "/seating",
+    name: "Seating",
+    pic: "./seating.jpg",
+    id: 7,
+  },
+  {
+    path: "/storage",
+    name: "Storage",
+    pic: "./storage.jpg",
+    id: 8,
+  },
+  {
+    path: "/sculpture",
+    name: "Sculpture",
+    pic: "./sculpture.jpg",
+    id: 9,
+  },
+  {
+    path: "/silverwave",
+    name: "Silverwave",
+    pic: "Silverware.jpg",
+    id: 10,
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    pic: "./tables.jpg",
+    id: 11,
+  },
+  {
+    path: "/dining-room",
+    name: "Dining Room",
+    pic: "./diningRoom.jpg",
+    id: 12,
+  },
+  {
+    path: "/living-room",
+    name: "Living Room",
+    pic: "./livingRoom.jpg",
+    id: 13,
+  },
+  {
+    path: "/marbles",
+    name: "Marbles",
+    pic: "./marbles.jpg",
+    id: 14,
+  },
+  {
+    path: "/bed-room",
+    name: "Bed Room",
+    pic: "./bedroom.jpg",
+    id: 15,
+  },
+  {
+    path: "/crystal",
+    name: "Crystal",
+    pic: "./crystal.jpg",
+    id: 16,
+  },
 ];
 
 export const lighting = [
   {
-    id : 1,
-    name : "",
-    pic : "./Products/Lighting/1.jpg"
-
+    id: 1,
+    name: "",
+    pic: "./Products/Lighting/1.jpg",
   },
   {
-    id : 2,
-    name : "",
-    pic : "./Products/Lighting/2.jpg"
-
+    id: 2,
+    name: "",
+    pic: "./Products/Lighting/2.jpg",
   },
   {
-    id : 3,
-    name : "",
-    pic : "./Products/Lighting/3.jpg"
-
+    id: 3,
+    name: "",
+    pic: "./Products/Lighting/3.jpg",
   },
   {
-    id : 4,
-    name : "",
-    pic : "./Products/Lighting/4.jpg"
-
+    id: 4,
+    name: "",
+    pic: "./Products/Lighting/4.jpg",
   },
   {
-    id : 5,
-    name : "",
-    pic : "./Products/Lighting/5.jpg"
-
+    id: 5,
+    name: "",
+    pic: "./Products/Lighting/5.jpg",
   },
   {
-    id : 6,
-    name : "",
-    pic : "./Products/Lighting/6.jpg"
-
+    id: 6,
+    name: "",
+    pic: "./Products/Lighting/6.jpg",
   },
   {
-    id : 7,
-    name : "",
-    pic : "./Products/Lighting/7.jpg"
-
+    id: 7,
+    name: "",
+    pic: "./Products/Lighting/7.jpg",
   },
   {
-    id : 8,
-    name : "",
-    pic : "./Products/Lighting/8.jpg"
-
+    id: 8,
+    name: "",
+    pic: "./Products/Lighting/8.jpg",
   },
   {
-    id : 9,
-    name : "",
-    pic : "./Products/Lighting/9.jpg"
-
+    id: 9,
+    name: "",
+    pic: "./Products/Lighting/9.jpg",
   },
   {
-    id : 10,
-    name : "",
-    pic : "./Products/Lighting/10.jpg"
+    id: 10,
+    name: "",
+    pic: "./Products/Lighting/10.jpg",
   },
   {
-    id : 11,
-    name : "",
-    pic : "./Products/Lighting/11.jpg"
+    id: 11,
+    name: "",
+    pic: "./Products/Lighting/11.jpg",
   },
   {
-    id : 12,
-    name : "",
-    pic : "./Products/Lighting/12.jpg"
+    id: 12,
+    name: "",
+    pic: "./Products/Lighting/12.jpg",
   },
   {
-    id : 13,
-    name : "",
-    pic : "./Products/Lighting/13.jpg"
+    id: 13,
+    name: "",
+    pic: "./Products/Lighting/13.jpg",
   },
   {
-    id : 14,
-    name : "",
-    pic : "./Products/Lighting/14.jpg"
+    id: 14,
+    name: "",
+    pic: "./Products/Lighting/14.jpg",
   },
   {
-    id : 15,
-    name : "",
-    pic : "./Products/Lighting/15.jpg"
+    id: 15,
+    name: "",
+    pic: "./Products/Lighting/15.jpg",
   },
   {
-    id : 16,
-    name : "",
-    pic : "./Products/Lighting/16.jpg"
+    id: 16,
+    name: "",
+    pic: "./Products/Lighting/16.jpg",
   },
   {
-    id : 17,
-    name : "",
-    pic : "./Products/Lighting/17.jpg"
+    id: 17,
+    name: "",
+    pic: "./Products/Lighting/17.jpg",
   },
   {
-    id : 18,
-    name : "",
-    pic : "./Products/Lighting/18.jpg"
+    id: 18,
+    name: "",
+    pic: "./Products/Lighting/18.jpg",
   },
   {
-    id : 19,
-    name : "",
-    pic : "./Products/Lighting/19.jpg"
+    id: 19,
+    name: "",
+    pic: "./Products/Lighting/19.jpg",
   },
   {
-    id : 20,
-    name : "",
-    pic : "./Products/Lighting/20.jpg"
+    id: 20,
+    name: "",
+    pic: "./Products/Lighting/20.jpg",
   },
   {
-    id : 21,
-    name : "",
-    pic : "./Products/Lighting/21.jpg"
+    id: 21,
+    name: "",
+    pic: "./Products/Lighting/21.jpg",
   },
   {
-    id : 22,
-    name : "",
-    pic : "./Products/Lighting/22.jpg"
+    id: 22,
+    name: "",
+    pic: "./Products/Lighting/22.jpg",
   },
-]
-
+  {
+    id: 23,
+    name: "",
+    pic: "./Products/Lighting/23.jpg",
+  },
+  {
+    id: 24,
+    name: "",
+    pic: "./Products/Lighting/24.jpg",
+  },
+  {
+    id: 25,
+    name: "",
+    pic: "./Products/Lighting/25.jpg",
+  },
+  {
+    id: 26,
+    name: "",
+    pic: "./Products/Lighting/26.jpg",
+  },
+  {
+    id: 27,
+    name: "",
+    pic: "./Products/Lighting/27.jpg",
+  },
+  {
+    id: 28,
+    name: "",
+    pic: "./Products/Lighting/28.jpg",
+  },
+  {
+    id: 29,
+    name: "",
+    pic: "./Products/Lighting/29.jpg",
+  },
+  {
+    id: 30,
+    name: "",
+    pic: "./Products/Lighting/30.jpg",
+  },
+  {
+    id: 31,
+    name: "",
+    pic: "./Products/Lighting/31.jpg",
+  },
+  {
+    id: 32,
+    name: "",
+    pic: "./Products/Lighting/32.jpg",
+  },
+  {
+    id: 33,
+    name: "",
+    pic: "./Products/Lighting/33.jpg",
+  },
+  {
+    id: 34,
+    name: "",
+    pic: "./Products/Lighting/34.jpg",
+  },
+  {
+    id: 35,
+    name: "",
+    pic: "./Products/Lighting/35.jpg",
+  },
+  {
+    id: 36,
+    name: "",
+    pic: "./Products/Lighting/36.jpg",
+  },
+  {
+    id: 37,
+    name: "",
+    pic: "./Products/Lighting/37.jpg",
+  },
+  {
+    id: 38,
+    name: "",
+    pic: "./Products/Lighting/38.jpg",
+  },
+  {
+    id: 39,
+    name: "",
+    pic: "./Products/Lighting/39.jpg",
+  },
+  {
+    id: 40,
+    name: "",
+    pic: "./Products/Lighting/40.jpg",
+  },
+  {
+    id: 41,
+    name: "",
+    pic: "./Products/Lighting/41.jpg",
+  },
+  {
+    id: 42,
+    name: "",
+    pic: "./Products/Lighting/42.jpg",
+  },
+  {
+    id: 43,
+    name: "",
+    pic: "./Products/Lighting/43.jpg",
+  },
+  {
+    id: 44,
+    name: "",
+    pic: "./Products/Lighting/44.jpg",
+  },
+  {
+    id: 45,
+    name: "",
+    pic: "./Products/Lighting/45.jpg",
+  },
+  {
+    id: 46,
+    name: "",
+    pic: "./Products/Lighting/46.jpg",
+  },
+  {
+    id: 47,
+    name: "",
+    pic: "./Products/Lighting/47.jpg",
+  },
+  {
+    id: 48,
+    name: "",
+    pic: "./Products/Lighting/48.jpg",
+  },
+  {
+    id: 49,
+    name: "",
+    pic: "./Products/Lighting/49.jpg",
+  },
+];
